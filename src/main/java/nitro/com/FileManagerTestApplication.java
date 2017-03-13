68a9d5e4a423aa41396e6ed5577588be9a6efdc8
Strekalovskyi D <ortin@i.ua>
2017-03-13 11:24:05 +0200

    finish

--- src/main/java/nitro/com/FileManagerTestApplication.java
+++ src/main/java/nitro/com/FileManagerTestApplication.java
@@ -2,12 +2,10 @@ package nitro.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("nitro.com")
-@EntityScan({"nitro.com.Entity;"})
+//@ComponentScan("nitro.com")
+//@EntityScan({"nitro.com.Entity;"})
 
 public class FileManagerTestApplication {
 
